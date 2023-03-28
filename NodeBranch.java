7406132e17f9afa7b3994a09e55995c01fcf7a25
Loic-Delbarre <Zhf3PzewgSoGtzlofybR@protonmail.com>
2023-03-28 17:45:38 +0200

    supp coucou

--- NodeBranch.java
+++ NodeBranch.java
@@ -4,9 +4,7 @@ public class NodeBranch extends Node {
 
     public NodeBranch(Node positive , Node negative,String data) {
         super(data);
-        System.out.println("coucou");
         this.positive=positive;
-        System.out.println("couddcou");
         this.negative=negative;
 
     }
