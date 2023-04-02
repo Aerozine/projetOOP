801662bb766c9d61b844579a22d51d0923aa884e
Loic-Delbarre <Zhf3PzewgSoGtzlofybR@protonmail.com>
2023-04-02 11:48:51 +0200

    got vectored

--- Node.java
+++ Node.java
@@ -7,7 +7,6 @@ public class Node {
     }
     public Node(){
         this.data=null;
-        //coucou
         
     }
 }
--- tree.java
+++ tree.java
@@ -1,33 +1,42 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Vector;
+
 //TODO
 // implementing a reshape function w/ new question , and 2 answer ( must search in the tree if answer == other answer)
-// switching for vector ?
+
 public class tree {
 
-public Node[] nodeArray;
+public Vector<Node> nodeArray;
+//got vectored
     public tree(String Filename) throws FileNotFoundException {
         Scanner scanner ;
         scanner = new Scanner(new File(Filename));
         int numberOfLine = scanner.nextInt();
         /* FIXME , what is the utility of this text? */
         System.out.println(scanner.nextLine());
-        this.nodeArray = new Node[numberOfLine];
+        this.nodeArray = new Vector<Node>(numberOfLine);
+        // numberOfLine element not set ? -> equivalent to add numberOfLine element with add
         for (int i = 0; i < numberOfLine; i++)
-            this.nodeArray[i] = new Node();
+            this.nodeArray.add(new Node());
         // init an array of Node
         // if a node is a question , then link to the object in the array
         for (int i = 0; scanner.hasNextLine(); i++) {
             // if it s a question , set the element as a branch and set theirs branch
             String type = scanner.next();
             if (type.equals("?")) {
-                this.nodeArray[i] = new NodeBranch(this.nodeArray[scanner.nextInt() - 1], this.nodeArray[scanner.nextInt() - 1], scanner.nextLine());
+                this.nodeArray.set(i ,new NodeBranch(this.nodeArray.get(scanner.nextInt() - 1), this.nodeArray.get(scanner.nextInt() - 1), scanner.nextLine()));
                 } else if (type.equals("=")) {
-                this.nodeArray[i] = new Node(scanner.nextLine());
+                this.nodeArray.set(i,new Node(scanner.nextLine()));
                 }
 
 
         }
+    }
+    public void reshape(Node indice,String newQuestion , String alternativeAnswer){
+    //TODO
+    //does the node indice the better way to specify the place to modify first?
+        // using .add() to add an element and set or setElementAt() to modify a certain element
     }
 }
