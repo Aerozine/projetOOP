f2fc9cc7718658f698ad7db404de0b9e0779e732
Loic-Delbarre <Zhf3PzewgSoGtzlofybR@protonmail.com>
2023-04-15 05:17:13 +0200

    end

--- game.java
+++ game.java
@@ -1,57 +1,81 @@
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class game {
-    public static void main(String[] args) throws Exception {
-        tree gametree = new tree("textfile.txt");
-        String answer;
+    public static void main(String[] args)
+    {
+        tree gametree;
+        try
+        {
+            gametree = new tree("textfile.txt");
+        } catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+        String answer = "y";
         Scanner scanner = new Scanner(System.in);
-        do {
-            rungame(gametree);
+        do
+        {
+            if (answer.equals("y")) runGame(gametree);
             System.out.println("wanna retry ?");
             answer = scanner.nextLine();
-        } while (answer.equals("y") || answer.equals("n"));
+        } while (!answer.equals("n"));
         scanner.close();
     }
 
-    public static void rungame(tree gametree) {
+    public static void runGame(tree gametree)
+    {
         System.out.println(gametree.getInitext());
         Node node = gametree.getFirstNode();
         Node prev = node;
         Scanner scanner = new Scanner(System.in);
         String answer;
-        while (node instanceof NodeBranch) {
-            do {
+        // while it's a question
+        while (node instanceof NodeBranch)
+        {
+            do
+            {
                 System.out.println(node.getData());
                 answer = scanner.nextLine();
             } while (!answer.equals("y") && !answer.equals("n"));
-
-            if (answer.equals("y")) {
+            if (answer.equals("y"))
+            {
                 prev = node;
                 node = ((NodeBranch) node).getPositive();
-            } else {
+            } else
+            {
                 prev = node;
                 node = ((NodeBranch) node).getNegative();
             }
+            if (node == null)
+                throw new IllegalArgumentException("the game tree is incorrect at " + (gametree.getIndex(prev) + 1));
         }
-        do {
+        do
+        {
             System.out.println("Does it is" + node.getData());
             answer = scanner.nextLine();
         } while (!answer.equals("y") && !answer.equals("n"));
-        if (answer.equals("n")) {
+        if (answer.equals("n"))
+        {
             System.out.println("I am unable to guess, you have won !");
             System.out.println("What did you chose ?");
             String newValue = scanner.nextLine();
             System.out.println("What question could I ask to distinguish a " + newValue + " from a " + node.getData() + " ?");
             String newQuestion = scanner.nextLine();
-            do {
+            do
+            {
                 System.out.println("For a " + newValue + ", would you answer yes or no to this question ?");
                 answer = scanner.nextLine();
             } while (!answer.equals("y") && !answer.equals("n"));
-            gametree.reshape(prev, node, newQuestion, newValue, answer.equals("y"));
+            try
+            {
+                //reshaping the tree
+                gametree.reshape(prev, node, newQuestion, newValue, answer.equals("y"));
+            } catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
 
         } else System.out.println("bingo");
-
-        scanner.close();
     }
 }
--- tree.java
+++ tree.java
@@ -1,116 +1,117 @@
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 import java.util.Vector;
+
 public class tree {
     private final String initext;
     private final Vector<Node> nodeArray;
-    public tree(String Filename) throws Exception {
+    private final String Filename;
+
+
+    public tree(String Filename) throws Exception
+    {
+        this.Filename = Filename;
         Scanner scanner;
         scanner = new Scanner(new File(Filename));
         int numberOfLine = scanner.nextInt();
-        this.initext = scanner.nextLine();
-        this.nodeArray = new Vector<>(numberOfLine);
-        Stack<Integer> cords = new Stack<>(); // store the number who link to another node
-        while(scanner.hasNextLine()) {
-            // if it s a question , set the element as a branch and set theirs branch
+        initext = scanner.nextLine();
+        nodeArray = new Vector<>(numberOfLine);
+        Stack<Integer> cords = new Stack<>(); // store the number who link to another node temporary
+        while (scanner.hasNextLine())
+        {
+            // if it s a question, set the element as a branch and set theirs branch
             String type = scanner.next();
-            if (type.equals("?")) {
+            if (type.equals("?"))
+            {
                 cords.push(scanner.nextInt() - 1);
                 cords.push(scanner.nextInt() - 1);
-                this.nodeArray.add( new NodeBranch(scanner.nextLine()));
-            } else if (type.equals("=")) {
-                this.nodeArray.add( new Node(scanner.nextLine()));
-            }else {
+                nodeArray.add(new NodeBranch(scanner.nextLine()));
+            } else if (type.equals("="))
+            {
+                nodeArray.add(new Node(scanner.nextLine()));
+            } else
+            {
+                // throw an error if it's not a = or a?
                 throw new IllegalArgumentException("a line isn't a question nor a response");
             }
         }
-        //create link
-        for (int i = numberOfLine - 1; i >= 0; i--) {
-            if (this.nodeArray.get(i) instanceof NodeBranch && !cords.empty()) {
-                Node node = this.nodeArray.get(i);
-                ((NodeBranch) node).setNegative(this.nodeArray.get(cords.pop()));
-                ((NodeBranch) node).setPositive(this.nodeArray.get(cords.pop()));
+        //create link between elements in array
+        for (int i = numberOfLine - 1; i >= 0; i--)
+        {
+            if (nodeArray.get(i) instanceof NodeBranch && !cords.empty())
+            {
+                Node node = nodeArray.get(i);
+                ((NodeBranch) node).setNegative(nodeArray.get(cords.pop()));
+                ((NodeBranch) node).setPositive(nodeArray.get(cords.pop()));
             }
         }
 
         scanner.close();
     }
 
-    public String getInitext() {
+    public String getInitext()
+    {
         return initext;
     }
 
-    public Node getFirstNode() {
+    public Node getFirstNode()
+    {
         return nodeArray.get(0);
     }
-    //to beautify but it works
-    public void reshape(Node question, Node Answer, String newQuestion, String alternativeAnswer, boolean alternativeIsYes) {
-        this.nodeArray.add(new Node(alternativeAnswer));
-        if (alternativeIsYes) this.nodeArray.add(new NodeBranch(nodeArray.lastElement(), Answer, newQuestion));
-        else this.nodeArray.add(new NodeBranch(Answer, nodeArray.lastElement(), newQuestion));
 
-        if (((NodeBranch) question).getPositive() == Answer) {
-            ((NodeBranch) question).setPositive(nodeArray.lastElement());
-        } else {
-            ((NodeBranch) question).setNegative(nodeArray.lastElement());
-        }
-        actualizeTheFile((NodeBranch) question);
+    public int getIndex(Node node)
+    {
+        return nodeArray.indexOf(node);
     }
-// somme changes need to be made
-    private void actualizeTheFile(NodeBranch question) {
 
-        int index = this.nodeArray.indexOf(question);
-        // jump to the line
-        // check if info is correct and if-not(pos or neg !=) rewrite the good version
-        // after that , go to the end of the file and write the 2 new nodes
-        int size = this.nodeArray.size();
+    //to beautify but it works
+    public void reshape(Node question, Node Answer, String newQuestion, String alternativeAnswer, boolean alternativeIsYes) throws IOException
+    {
+        //creating the new node
+        nodeArray.add(new Node(alternativeAnswer));
+        //depending on if the new value is positive or negative, create a new question
+        if (alternativeIsYes) nodeArray.add(new NodeBranch(nodeArray.lastElement(), Answer, newQuestion));
+        else nodeArray.add(new NodeBranch(Answer, nodeArray.lastElement(), newQuestion));
+        //modify the link to the question to the new question
+        if (((NodeBranch) question).getPositive() == Answer)
+            ((NodeBranch) question).setPositive(nodeArray.lastElement());
+        else ((NodeBranch) question).setNegative(nodeArray.lastElement());
+        //update the file
+        updateFile((NodeBranch) question);
+    }
 
-        //renaming Filename will overwrite the textfile.txt and modify the game (make a copy of the original textfile before this)
-        File file = new File("file.txt");
-        BufferedReader reader = null;
-        RandomAccessFile randomAccessFile = null;
-        String line = null;
-        StringBuilder stringBuilder = new StringBuilder();
-        int lineNumber = 0;
-
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            while ((line = reader.readLine()) != null) {
-                lineNumber++;
-                if (lineNumber == index + 1) {
-
-                    int pos = this.nodeArray.indexOf(question.getPositive());
-                    int neg = this.nodeArray.indexOf(question.getNegative());
-                    //rewrite the line and append it for the question
-                    //the parsing must be rewritten here
-                    stringBuilder.append("new line of question");
-                } else {
-                    stringBuilder.append(line);
-                }
-                stringBuilder.append(System.lineSeparator());
-            }// append store the whole file on an array , does it is the best way to do it ?
-            //should we use an RandomAccessFile ?
-            //when readLine==null ,append the 2 new node
-            //the parsing must be rewritten here
-            stringBuilder.append("new node 1");
-            stringBuilder.append(System.lineSeparator());
-            stringBuilder.append("new node 2");
-            stringBuilder.append(System.lineSeparator());
-            // Write the modified content back to the file
-            //need to clarify try catch and throw in the code
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            // Close the reader and randomAccessFile
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    private String NodeToString(Node q)
+    {
+        if (q instanceof NodeBranch)
+        {
+            // get the positive and negative node values
+            int pos = nodeArray.indexOf(((NodeBranch) q).getPositive()) + 1;
+            int neg = nodeArray.indexOf(((NodeBranch) q).getNegative()) + 1;
+            return "? " + pos + " " + neg + " " + q.getData();
         }
+        return "= " + q.getData();
+    }
 
+    public void updateFile(NodeBranch question) throws IOException
+    {
+        //+1 because we have the inittext
+        int index = nodeArray.indexOf(question) + 1;
+        //read all the files
+        Path path = Paths.get(Filename);
+        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        //modifying the question node
+        lines.set(index, NodeToString(question));
+        // adding the new question and the new answer
+        lines.add(NodeToString(nodeArray.get(nodeArray.size() - 2)));
+        lines.add(NodeToString(nodeArray.lastElement()));
+        //write all changes
+        Files.write(path, lines, StandardCharsets.UTF_8);
     }
 }
